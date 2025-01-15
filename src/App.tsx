f8f2bde9788ab3fbff5f309c004b20569463c8d4
dangqbao23 <dangqbao23@gmail.com>
2025-01-15 22:39:14 +0700

    Update file App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { Button } from "@mui/material"
+import { Button, TextField } from "@mui/material"
 
 function App() {
 
   return (
     <>
       <p>Hello world</p>
+      <TextField id="outlined-basic" label="test" variant="outlined" size="small" />
       <Button variant="contained">Primary</Button>
     </>
   )
