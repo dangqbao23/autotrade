4f78fcea8f7d37029e68e799d8b0427505ee3fd5
dangqbao23 <dangqbao23@gmail.com>
2025-02-16 22:26:07 +0700

    update code Codeium

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import {
   Box,
   Button,
   FormControl,
-  Grid,
   Grid2,
   InputLabel,
   MenuItem,
@@ -12,50 +11,49 @@ import {
 import React from "react";
 
 function App() {
-  const [san, setSan] = React.useState("");
+  const [selectedSan, setSelectedSan] = React.useState("");
 
-  const handleChange = (event: SelectChangeEvent) => {
-    setSan(event.target.value as string);
+  const handleSanChange = (event: SelectChangeEvent<string>) => {
+    setSelectedSan(event.target.value);
   };
 
-  const handleClick = () => {
-    console.log(san);
+  const handleConnectClick = () => {
+    console.log(selectedSan);
   };
+
   return (
-    <>
-      <Box sx={{ flexGrow: 1, p: 1 }}>
-        <Grid2 container>
-          <Grid2 size={1}>
-            <Box sx={{ minWidth: 120 }}>
-              <FormControl size="small" fullWidth>
-                <InputLabel id="demo-simple-select-label">San</InputLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={san}
-                  label="San"
-                  onChange={handleChange}
-                >
-                  <MenuItem value={"Binance"}>Binance</MenuItem>
-                  <MenuItem value={"Gateio"}>Gate IO</MenuItem>
-                  <MenuItem value={"OKEX"}>OK</MenuItem>
-                </Select>
-              </FormControl>
-            </Box>
-          </Grid2>
-          <Grid2 size={1}>
-            <Button
-              variant="contained"
-              color="success"
-              size="medium"
-              onClick={handleClick}
-            >
-              Connect
-            </Button>
-          </Grid2>
+    <Box sx={{ flexGrow: 1, p: 1 }}>
+      <Grid2 container>
+        <Grid2 size={1}>
+          <Box sx={{ minWidth: 120 }}>
+            <FormControl size="small" fullWidth>
+              <InputLabel id="demo-simple-select-label">Select San</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={selectedSan}
+                label="Select San"
+                onChange={handleSanChange}
+              >
+                <MenuItem value={"Binance"}>Binance</MenuItem>
+                <MenuItem value={"Gateio"}>Gate IO</MenuItem>
+                <MenuItem value={"OKEX"}>OK</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
         </Grid2>
-      </Box>
-    </>
+        <Grid2 size={1}>
+          <Button
+            variant="contained"
+            color="success"
+            size="medium"
+            onClick={handleConnectClick}
+          >
+            Connect
+          </Button>
+        </Grid2>
+      </Grid2>
+    </Box>
   );
 }
 
